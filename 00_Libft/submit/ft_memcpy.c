e92b578ceaf8865c2eb48c9585424fc4700f29f5
yu-ri <yuri2517300028@gmail.com>
2025-10-18 23:35:45 +0900

    makede branch with NULL guard

--- 00_Libft/submit/ft_memcpy.c
+++ 00_Libft/submit/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yikebata  <yikebata@student.42tokyo.>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 08:59:35 by yikebata          #+#    #+#             */
-/*   Updated: 2025/10/16 17:28:18 by yikebata         ###   ########.fr       */
+/*   Updated: 2025/10/18 23:26:28 by yu-ri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const unsigned char	*s;
 	size_t				i;
 
-	if (src == NULL || dest == NULL)
-		return (NULL);
+//	if (src == NULL || dest == NULL)
+//		return (NULL);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	i = 0;
