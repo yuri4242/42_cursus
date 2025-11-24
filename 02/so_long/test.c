200714f7c78ba40913a551469f264435a7850c55
yikebata <yikebata@student.42tokyo.jp>
2025-11-24 17:45:26 +0900

    20251124 added from 42

---
+++ 02/so_long/test.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yikebata <yikebata@student.42tokyo.jp      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 12:19:19 by yikebata          #+#    #+#             */
+/*   Updated: 2025/11/24 12:19:22 by yikebata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx/mlx.h"
+
+int main(void)
+{
+    void    *mlx;
+    void    *win;
+
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, 800, 600, "My First Window");
+    
+    mlx_loop(mlx);
+    return (0);
+}
